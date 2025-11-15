86bf7af89174662219ce8e105b1a633562c17edf
JairAmado08 <jairrenatoamadodiaz@gmail.com>
2025-11-14 19:14:32 -0500

    Create init.py

---
+++ views/inventario/init.py
@@ -0,0 +1,20 @@
+"""
+Vistas del módulo de inventario
+"""
+from . import (
+    dashboard_view,
+    buscar_view,
+    registrar_view,
+    actualizar_view,
+    eliminar_view,
+    reportes_view
+)
+
+__all__ = [
+    'dashboard_view',
+    'buscar_view',
+    'registrar_view',
+    'actualizar_view',
+    'eliminar_view',
+    'reportes_view'
+]
