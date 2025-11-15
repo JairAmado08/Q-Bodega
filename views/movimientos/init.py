88ef317e42850c38ad04aeb8d985db7d79aca6ae
JairAmado08 <jairrenatoamadodiaz@gmail.com>
2025-11-14 19:19:47 -0500

    Create init.py

---
+++ views/movimientos/init.py
@@ -0,0 +1,18 @@
+"""
+Vistas del módulo de movimientos
+"""
+from . import (
+    movimientos_dashboard_view,
+    buscar_movimiento_view,
+    registrar_movimiento_view,
+    actualizar_movimiento_view,
+    eliminar_movimiento_view
+)
+
+__all__ = [
+    'movimientos_dashboard_view',
+    'buscar_movimiento_view',
+    'registrar_movimiento_view',
+    'actualizar_movimiento_view',
+    'eliminar_movimiento_view'
+]
