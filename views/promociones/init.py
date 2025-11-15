caba963f3fad2ed22bbbb52c42d8d92cc89bbeed
JairAmado08 <jairrenatoamadodiaz@gmail.com>
2025-11-14 23:11:08 -0500

    Update init.py

--- views/promociones/init.py
+++ views/promociones/init.py
@@ -1,1 +1,18 @@
+"""
+Vistas del módulo de promociones
+"""
+from . import (
+    promociones_dashboard_view,
+    registrar_promocion_view,
+    buscar_promocion_view,
+    actualizar_promocion_view,
+    eliminar_promocion_view
+)
 
+__all__ = [
+    'promociones_dashboard_view',
+    'registrar_promocion_view',
+    'buscar_promocion_view',
+    'actualizar_promocion_view',
+    'eliminar_promocion_view'
+]
