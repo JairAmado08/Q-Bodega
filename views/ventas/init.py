1b8394cade2ff51cbacfad043e5443ffa803e450
JairAmado08 <jairrenatoamadodiaz@gmail.com>
2025-11-16 19:37:06 -0500

    Update init.py

--- views/ventas/init.py
+++ views/ventas/init.py
@@ -5,12 +5,14 @@ from . import (
     ventas_dashboard_view,
     registrar_venta_view,
     buscar_venta_view,
-    detalle_venta_view
+    detalle_venta_view,
+    devolucion_view
 )
 
 __all__ = [
     'ventas_dashboard_view',
     'registrar_venta_view',
     'buscar_venta_view',
-    'detalle_venta_view'
+    'detalle_venta_view',
+    'devolucion_view'
 ]
